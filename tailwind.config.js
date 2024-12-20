857b339b21364b040311930fa790c6318650d56e
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-20 17:42:44 +0900

    tailwind 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      pretendard: ['Pretendard-Regular', 'sans-serif'],
+      BMJUA: ['BMJUA', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        broccoli: '#4CAF50',
+        inactive: '#B8B8B8',
+      },
+    },
+    boxShadow: {
+      button: '0 2px 4px rgba(0,0,0,0.25) ',
+    },
   },
   plugins: [],
-}
-
+};
