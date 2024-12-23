c228be8ae12407f7302fd855a6414b833a831137
guswhdleee <gksrmrtn123@naver.com>
2024-12-23 15:03:04 +0900

    🎨 feat[#34]: Modal 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className='max-w-[400px] m-auto'>
-    </div>
-  );
+  return <div className="max-w-[400px] m-auto"></div>;
 }
 
 export default App;
