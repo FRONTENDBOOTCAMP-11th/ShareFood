8f32c24818f81e8cbdb15fe4dbbfbfc7302a681c
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-23 16:44:15 +0900

    feat: 로그인 상태 유지 버튼 UI

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -5,10 +5,14 @@ const Login: React.FC = () => {
     <>
       <div className="flex flex-col px-4 bg-main py-[120px]">
         <LoginSignupTitle>로그인</LoginSignupTitle>
-        <section className="bg-white mt-6 h-[372px] rounded-[10px] font-sans">
-          <div className="px-4 py-[27px] [&_input]:w-full [&_input]:h-[26px] [&_input]:border-b-[1px] [&_input]:border-[#CCCCCC] [&_input]:text-xs [&_input]:py-1 [&_input:focus]:outline-none">
+        <section className="bg-white mt-6 h-[372px] rounded-[10px] font-sans text-xs">
+          <div className="px-4 py-[27px] [&_input]:w-full [&_input]:h-[26px] [&_input]:border-b-[1px] [&_input]:border-[#CCCCCC] [&_input]:py-1 [&_input:focus]:outline-none">
             <input className="mb-4" type="email" placeholder="아이디(이메일)" />
             <input type="password" placeholder="비밀번호" />
+            <div className="mt-6 text-inactive flex items-center gap-2">
+              <img className="size-5" src="images/btnImg/checkInactive.svg" />
+              <p>로그인 상태 유지</p>
+            </div>
           </div>
         </section>
       </div>
