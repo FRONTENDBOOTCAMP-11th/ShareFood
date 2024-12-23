5ab5b22d5d441bf0a124a8ae8ce24ab43cdf9b9b
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-23 18:02:36 +0900

    💄 회원가입 이동 링크 완료, 에러메세지 공간 추가

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -6,16 +6,17 @@ const Login: React.FC = () => {
     <>
       <div className="flex flex-col px-4 bg-main py-[120px] my-10">
         <LoginSignupTitle>로그인</LoginSignupTitle>
-        <main className="bg-white mt-6 px-4 h-[372px] rounded-[10px] font-sans text-xs">
-          <section className=" py-[27px] [&_input]:w-full [&_input]:h-[26px] [&_input]:border-b-[1px] [&_input]:border-[#CCCCCC] [&_input]:py-1 [&_input:focus]:outline-none">
+        <main className="bg-white mt-6 px-4 py-[27px] h-[372px] rounded-[10px] font-sans text-xs">
+          <section className="[&_input]:w-full [&_input]:h-[26px] [&_input]:border-b-[1px] [&_input]:border-[#CCCCCC] [&_input]:py-1 [&_input:focus]:outline-none">
             <input className="mb-4" type="email" placeholder="아이디(이메일)" />
             <input type="password" placeholder="비밀번호" />
+            <p className="text-red-500 h-[21px] leading-[21px]">* 에러메세지</p>
             <div className="mt-6 text-subText flex items-center gap-2">
               <img className="size-5" src="images/btnImg/checkInactive.svg" />
               <p>로그인 상태 유지</p>
             </div>
           </section>
-          <section className="flex flex-col justify-center items-center gap-4">
+          <section className="flex flex-col justify-center items-center gap-4 mt-9">
             <Button
               width="332px"
               height="40px"
@@ -38,7 +39,11 @@ const Login: React.FC = () => {
               </div>
             </Button>
           </section>
-          <section></section>
+          <section className="my-7 text-center">
+            <a className="text-subText underline font-semibold " href="/">
+              회원가입하기
+            </a>
+          </section>
         </main>
       </div>
     </>
