111f16114001748512ffc2552d4dbb49ed7d9b1d
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-26 10:05:15 +0900

    💄 모서리 굴곡 추가

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -4,7 +4,7 @@ import LoginSignupTitle from '../../components/LoginSignupTitle';
 const Login: React.FC = () => {
   return (
     <>
-      <div className="flex flex-col px-4 bg-main py-[120px] my-10">
+      <div className="flex flex-col px-4 bg-main py-[120px] my-10 rounded-[10px]">
         <LoginSignupTitle>로그인</LoginSignupTitle>
         <main className="bg-white mt-6 px-4 py-[27px] h-[372px] rounded-[10px] font-sans text-xs">
           <section className="[&_input]:w-full [&_input]:h-[26px] [&_input]:border-b-[1px] [&_input]:border-[#CCCCCC] [&_input]:py-1 [&_input:focus]:outline-none">
