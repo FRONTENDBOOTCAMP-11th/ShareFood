3258b61b6273f7eb02b7b4de90e005d7e49ce0b5
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-26 16:32:05 +0900

    💄 로그인 페이지 색상명 통일

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -7,7 +7,7 @@ const Login: React.FC = () => {
       <div className="flex flex-col px-4 bg-main py-[120px] my-10 rounded-[10px]">
         <LoginSignupTitle>로그인</LoginSignupTitle>
         <main className="bg-white mt-6 px-4 py-[27px] h-[372px] rounded-[10px] font-sans text-xs">
-          <section className="[&_input]:w-full [&_input]:h-[26px] [&_input]:border-b-[1px] [&_input]:border-[#CCCCCC] [&_input]:py-1 [&_input:focus]:outline-none">
+          <section className="[&_input]:w-full [&_input]:h-[26px] [&_input]:border-b-[1px] [&_input]:border-line2 [&_input]:py-1 [&_input:focus]:outline-none">
             <input className="mb-4" type="email" placeholder="아이디(이메일)" />
             <input type="password" placeholder="비밀번호" />
             <p className="text-red-500 h-[21px] leading-[21px]">* 에러메세지</p>
@@ -40,7 +40,7 @@ const Login: React.FC = () => {
             </Button>
           </section>
           <section className="my-7 text-center">
-            <a className="text-subText underline font-semibold " href="/">
+            <a className="text-font2 underline font-semibold " href="/">
               회원가입하기
             </a>
           </section>
