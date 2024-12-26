ebb79cb54128fb77ba77ae212bc722eb2890298f
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-26 11:05:21 +0900

    💄 에러 메세지 글자 크기 지정

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,4 +1,9 @@
-const Error: React.FC<Props> = ({ children, text }) => {
+interface Props {
+  children: React.ReactNode;
+  text?: string;
+}
+
+const Error: React.FC<Props> = ({ children, text = 'text-xs' }) => {
   return <p className={`text-error ${text}`}>{children}</p>;
 };
 
