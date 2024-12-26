9da199f25c01e4522f323667221bf6aaccdb80bd
beofme31 <beofme31@naver.com>
2024-12-26 13:23:49 +0900

    💄feat[62]: 스플래시 애니메이션 적용 화면(컨벤션 적용)

--- src/pages/splash/splashTransition.tsx
+++ src/pages/splash/splashTransition.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import Splash from "./splash";
-import SplashStart from "./splashStart";
+import { useEffect, useState } from 'react;
+import Splash from './splash';
+import SplashStart from './splashStart';
 
 const SplashTransition: React.FC = () => {
   const [splash, setSplash] = useState(true);
