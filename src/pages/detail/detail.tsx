ac46b38602d51ed1a329eb0b8a869883a9529293
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-27 14:43:54 +0900

    💄 modal 내용 페이지에서 작성

--- src/pages/detail/detail.tsx
+++ src/pages/detail/detail.tsx
@@ -28,14 +28,15 @@ const Detail = () => {
   const [viewPayment, setViewPayment] = useState(false);
 
   // counter 상태 관리
-  const [num, setNum] = useState(0);
+  // const [num, setNum] = useState(0);
 
   // modal 상태 관리
-  // 임시로 1, 2, 3으로 지정했고 추후에 상세페이지에서 버튼 클릭했을 때
-  // 정보를 받아서 모달 콘텐츠가 알맞게 나오게 하면 될 것 같습니다.
-  const [content, setContent] = useState(2);
+  // 상세페이지에서 버튼 클릭했을 때 정보를 받아서 모달 콘텐츠가 알맞게 나오게 하면 될 것 같습니다.
+  // 임시로 공구하기, 구매자 확인만 동작하게 만들었습니다.
+  const [content, setContent] = useState(typeBuy);
 
-  const handleModal = () => {
+  const handleModal = (contentType: string) => {
+    setContent(contentType);
     setViewPayment(true);
   };
 
@@ -113,7 +114,8 @@ const Detail = () => {
             text="text-sm"
             bg="main"
             color="white"
-            onClick={() => handleModal()}
+            // handleModal 변수에 따라서 모달 내용 변경되게
+            onClick={() => handleModal(typeBuy)}
           >
             공구하기
           </Button>
@@ -121,24 +123,24 @@ const Detail = () => {
         {viewPayment && (
           <Modal setViewPayment={setViewPayment}>
             {/* content에 입력된 정보에 따라서 modal 내용이 변경될 수 있게 */}
-            {/* content === 1 : 공구 */}
-
-            <div>
-              <h2 className="mb-5 font-semibold">
-                공구 장소, 시간을 확인하세요
-              </h2>
-              <div className="flex flex-col gap-4 mb-8">
-                <Tag tagName="location">공릉2동 주공아파트 3단지 놀이터 앞</Tag>
-                <Tag tagName="time">10:00</Tag>
-                <Tag tagName="people">2/10</Tag>
+            {content === typeBuy && (
+              <div>
+                <h2 className="mb-5 font-semibold">
+                  공구 장소, 시간을 확인하세요
+                </h2>
+                <div className="flex flex-col gap-4 mb-8">
+                  <Tag tagName="location">
+                    공릉2동 주공아파트 3단지 놀이터 앞
+                  </Tag>
+                  <Tag tagName="time">10:00</Tag>
+                  <Tag tagName="people">2/10</Tag>
+                </div>
+                <Button bg="main" color="white" height="40px" text="text-sm">
+                  공구하기
+                </Button>
               </div>
-              <Button bg="main" color="white" height="40px" text="text-sm">
-                공구하기
-              </Button>
-            </div>
-
-            {/* content === 2 : 구매 */}
-            {/* {content === 2 && (
+            )}
+            {/* {content === ??? && (
                 <div>
                   <h2 className="mb-5 font-semibold">
                     거래 가격, 장소, 개수를 확인하세요
@@ -159,37 +161,36 @@ const Detail = () => {
                 </div>
               )} */}
             {/* 구매자 늘어나면 스크롤 넣어야 될것 같습니다. */}
-            {/* content === 3 : 구매자 조회 */}
-            {/* {content === 3 && (
-                <div>
-                  <h2 className="mb-3">구매 신청자 목록</h2>
-                  <div className="flex flex-col gap-5">
-                    <Tag tagName="people">2/10</Tag>
-                    <ul className="mr-7 flex flex-col gap-3">
-                      <li className="flex items-center gap-3">
-                        <img src="images/logos/default_image.svg" />
-                        <p className="grow text-left">장유진</p>
-                        <Tag tagName="item">1/6</Tag>
-                      </li>
-                      <li className="flex items-center gap-3">
-                        <img src="images/logos/default_image.svg" />
-                        <p className="grow text-left">이선재</p>
-                        <Tag tagName="item">1/6</Tag>
-                      </li>
-                      <li className="flex items-center gap-3">
-                        <img src="images/logos/default_image.svg" />
-                        <p className="grow text-left">이현종</p>
-                        <Tag tagName="item">1/6</Tag>
-                      </li>
-                      <li className="flex items-center gap-3">
-                        <img src="images/logos/default_image.svg" />
-                        <p className="grow text-left">김건우</p>
-                        <Tag tagName="item">1/6</Tag>
-                      </li>
-                    </ul>
-                  </div>
+            {content === typeSell && (
+              <div>
+                <h2 className="mb-3">구매 신청자 목록</h2>
+                <div className="flex flex-col gap-5">
+                  <Tag tagName="people">2/10</Tag>
+                  <ul className="mr-7 flex flex-col gap-3">
+                    <li className="flex items-center gap-3">
+                      <img src="images/logos/default_image.svg" />
+                      <p className="grow text-left">장유진</p>
+                      <Tag tagName="item">1/6</Tag>
+                    </li>
+                    <li className="flex items-center gap-3">
+                      <img src="images/logos/default_image.svg" />
+                      <p className="grow text-left">이선재</p>
+                      <Tag tagName="item">1/6</Tag>
+                    </li>
+                    <li className="flex items-center gap-3">
+                      <img src="images/logos/default_image.svg" />
+                      <p className="grow text-left">이현종</p>
+                      <Tag tagName="item">1/6</Tag>
+                    </li>
+                    <li className="flex items-center gap-3">
+                      <img src="images/logos/default_image.svg" />
+                      <p className="grow text-left">김건우</p>
+                      <Tag tagName="item">1/6</Tag>
+                    </li>
+                  </ul>
                 </div>
-              )} */}
+              </div>
+            )}
           </Modal>
         )}
       </div>
