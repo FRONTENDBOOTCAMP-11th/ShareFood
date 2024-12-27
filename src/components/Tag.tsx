7adf5d881e39817c92b8dc0cfaa11afa6e49fd2b
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-27 10:46:47 +0900

    💄 tag 기본 마진 제거

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 const Tag: React.FC<Props> = ({ children, tagName }) => {
   return (
-    <div className="flex items-center gap-[10px] ml-2 font-sans">
+    <div className="flex items-center gap-[10px] font-sans">
       <img className="size-5" src={`/images/tagImg/${tagName}.svg`} />
       {children}
     </div>
