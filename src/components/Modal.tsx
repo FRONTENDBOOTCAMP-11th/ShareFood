27276072145bd8791784910b75d3aaa17ff81103
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-27 17:49:11 +0900

    💄 modal 위치 중앙으로 수정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,14 +13,13 @@ function Modal({ setViewPayment, children }: props) {
 
   return (
     <div className="fixed justify-normal top-0 left-50 w-[448px] h-full bg-black bg-opacity-20 overflow-hidden">
-      <button
-        className="fixed top-16 right-40 w-4 h-4 text-3xl text-neutral-50 cursor-pointer"
-        onClick={() => closeModal()}
-      >
-        X
-      </button>
-      {/* <div className="absolute top-[84%] left-2/4 w-[400px] h-1/3 p-5 text-center bg-white rounded shadow transform -translate-x-1/2 -translate-y-1/2 animate-revealDown"> */}
-      <div className="absolute bottom-0 left-1/2 w-[400px] p-5 text-center bg-white rounded shadow transform -translate-x-1/2 animate-revealDown mb-24">
+      <div className="absolute bottom-1/3 left-1/2 w-[400px] p-5 bg-white rounded shadow transform -translate-x-1/2 animate-revealDown ">
+        <button
+          className="fixed top-4 right-2 size-6 text-3xl text-neutral-50 cursor-pointer"
+          onClick={() => closeModal()}
+        >
+          <img src="images/icons/close.svg" />
+        </button>
         {/* 아래부터 모달 내용 */}
         {children}
       </div>
