0e5c12e6c4c64cd145e5467643a58ab1c32c7bc1
Youjiiin <yujinjang24@gmail.com>
2024-12-30 10:19:16 +0900

    ✨ axiosInstance, authStore 기본 설정

---
+++ src/hooks/axiosInstance.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { useAuthStore } from '../store/authStore';
+
+const REQUEST_TIME = 25000;
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  timeout: REQUEST_TIME,
+  headers: {
+    'Content-Type': 'application/json', // request의 데이터 타입
+    accept: 'application/json', // response의 데이터 타입
+    'client-id': 'final07',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = useAuthStore.getState().user?.accessToken;
+    if (useAuthStore.getState().user?.accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  },
+);
---
+++ src/store/authStore.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand';
+
+export type User = {
+  accessToken: string;
+};
+
+interface AuthState {
+  user: User | null;
+}
+
+interface AuthActions {
+  setUser: (user: User | null) => void;
+  resetUser: (user: User | null) => void;
+}
+
+export const useAuthStore = create<AuthState & AuthActions>((set) => ({
+  user: null,
+  setUser: (user: User | null) => {
+    set({ user });
+  },
+  resetUser: () => set({ user: null }),
+}));
