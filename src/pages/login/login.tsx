9f9a32e75264e6977175ecdcf42a2b0a0d50a126
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-31 17:20:39 +0900

    ✨로그인 성공 시 메인 페이지로 이동

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Button from '../../components/Button';
 import Error from '../../components/Error';
 import LoginSignupTitle from '../../components/LoginSignupTitle';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 import { axiosInstance } from '../../hooks/axiosInstance';
 import { useForm } from 'react-hook-form';
@@ -18,6 +18,8 @@ const Login: React.FC = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>();
+
+  const navigate = useNavigate();
 
   // 로그인 상태 유지
   const [active, setActive] = useState<string>('inactive');
@@ -40,6 +42,7 @@ const Login: React.FC = () => {
     onSuccess: (res) => {
       console.log(res);
       alert(`${res.item.name} 님 환영합니다.`);
+      navigate('/main');
     },
   });
 
