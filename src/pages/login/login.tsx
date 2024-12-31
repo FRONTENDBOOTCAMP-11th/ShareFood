fd493d4bbde9e537feaab99f7267a4de23814b36
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-12-31 16:23:34 +0900

    ✨ 로그인 기능 기초

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,8 +3,18 @@ import Button from '../../components/Button';
 import Error from '../../components/Error';
 import LoginSignupTitle from '../../components/LoginSignupTitle';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
+import { axiosInstance } from '../../hooks/axiosInstance';
+import { useForm } from 'react-hook-form';
+
+interface FormData {
+  email: string;
+  password: string;
+}
 
 const Login: React.FC = () => {
+  const { handleSubmit } = useForm<FormData>();
+
   // 로그인 상태 유지
   const [active, setActive] = useState<string>('inactive');
 
@@ -12,6 +22,20 @@ const Login: React.FC = () => {
   const handleActive = () => {
     setActive((active) => (active === 'inactive' ? 'active' : 'inactive'));
   };
+
+  const login = useMutation({
+    mutationFn: async (formData: FormData) =>
+      axiosInstance.post('/users/login', formData),
+    onSuccess: (res) => {
+      console.log(res);
+
+      const user = res.data.item;
+
+      console.log(user);
+
+      alert(user.name + '님, 안녕하세요');
+    },
+  });
 
   return (
     <>
