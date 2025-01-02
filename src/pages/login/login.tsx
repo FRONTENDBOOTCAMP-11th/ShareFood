4d3c1cbc5156f06c128420d7ee663b0d0293ae43
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2025-01-02 14:42:45 +0900

    ✨ 로그인 상태 유지 버튼 활성화 될 때만 데이터 저장

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -43,18 +43,20 @@ const Login: React.FC = () => {
     onSuccess: (res) => {
       console.log(res);
 
-      const user = res.item;
-      console.log(user);
+      if (active === 'active') {
+        const user = res.item;
+        console.log(user);
 
-      setUser({
-        _id: user._id,
-        name: user.name,
-        profile: user.image ? user.image : undefined,
-        accessToken: user.token.accessToken,
-        refreshToken: user.token.refreshToken,
-      });
+        setUser({
+          _id: user._id,
+          name: user.name,
+          profile: user.image ? user.image : undefined,
+          accessToken: user.token.accessToken,
+          refreshToken: user.token.refreshToken,
+        });
 
-      console.log('Updated user:', useAuthStore.getState().user);
+        console.log('Updated user:', useAuthStore.getState().user);
+      }
 
       // 로그인 성공 시 알림창 띄우고 메인페이지 이동
       alert(`${res.item.name} 님 환영합니다.`);
