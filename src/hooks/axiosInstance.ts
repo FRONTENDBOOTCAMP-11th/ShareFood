56eaeb95f5ebc624adecb13d9ebbaff34df503d1
Youjiiin <yujinjang24@gmail.com>
2025-01-04 15:53:18 +0900

    🐛 axios Instance token 설정

--- src/hooks/axiosInstance.ts
+++ src/hooks/axiosInstance.ts
@@ -6,8 +6,8 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   timeout: REQUEST_TIME,
   headers: {
-    'Content-Type': 'application/json', // request의 데이터 타입
-    accept: 'application/json', // response의 데이터 타입
+    'Content-Type': 'application/json',
+    accept: 'application/json',
     'client-id': 'final07',
   },
 });
@@ -17,13 +17,11 @@ axiosInstance.interceptors.request.use(
     const accessToken =
       localStorage.getItem('accessToken') ||
       sessionStorage.getItem('accessToken');
-    const refreshToken =
-      localStorage.getItem('accessToken') ||
-      sessionStorage.getItem('accessToken');
 
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
+
     return config;
   },
   (error) => {
