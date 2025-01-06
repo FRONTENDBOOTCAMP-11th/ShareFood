ea00b1c9cc97a46751c98272baf14510193d025c
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2025-01-06 16:05:11 +0900

    ✨ 입력정보 없으면 에러메세지 출력

--- src/pages/write/write.tsx
+++ src/pages/write/write.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
+import { axiosInstance } from '../../hooks/axiosInstance';
 import Button from '../../components/Button';
 import Header from '../../components/Layout/Header';
 import ImageUpload from '../../components/ImageUpload';
@@ -9,11 +12,43 @@ import close from '/images/icons/close.svg';
 import TypeSelector from '../../components/TypeSelector';
 import Error from '../../components/Error';
 
+interface FormData {
+  title: string;
+  location: string;
+  time: string;
+  text: string;
+  type?: string;
+}
+
 const Write = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
   const navigate = useNavigate();
 
   const [productsType, setProductsType] = useState('buy');
   const [meetingLocation, setMeetingLocation] = useState('전체지역');
+
+  const addPost = useMutation({
+    mutationFn: async (formData: FormData) => {
+      formData.type = 'porduct';
+      const res = await axiosInstance.post('/product');
+      return res.data;
+    },
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (err) => {
+      console.error(err);
+    },
+  });
+
+  const onSubmit = (data: FormData) => {
+    addPost.mutate(data);
+  };
 
   return (
     <div className="min-h-screen bg-back1 pt-14 pb-[100px]">
@@ -38,7 +73,10 @@ const Write = () => {
           setProductsType={setProductsType}
         />
 
-        <form className="flex flex-col gap-[8px] text-[13px]">
+        <form
+          className="flex flex-col gap-[8px] text-[13px]"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className="info-title">
             <div className="flex gap-[22px] py-[7px] mb-[7px] border-b">
               <p className="font-semibold">제목 </p>
@@ -46,9 +84,12 @@ const Write = () => {
                 type="text"
                 className="outline-none grow"
                 placeholder="제목을 입력해주세요."
+                {...register('title', {
+                  required: '* 제목은 필수입니다',
+                })}
               />
             </div>
-            <Error children={'* 제목은 필수입니다'} />
+            <Error>{errors.title?.message}</Error>
           </div>
 
           <div className="info-location">
@@ -69,9 +110,12 @@ const Write = () => {
                 type="text"
                 className="outline-none text-xs grow"
                 placeholder="거래 상세 위치를 입력해주세요."
+                {...register('location', {
+                  required: '* 상세 위치는 필수입니다.',
+                })}
               />
             </div>
-            <Error children={'* 상세 위치는 필수입니다.'} />
+            <Error>{errors.location?.message}</Error>
           </div>
 
           <div className="info-time">
@@ -81,9 +125,10 @@ const Write = () => {
                 type="text"
                 className="outline-none text-xs grow"
                 placeholder="마감 시간을 입력해주세요."
+                {...register('time', { required: '* 마감시간은 필수입니다' })}
               />
             </div>
-            <Error children={'* 마감시간은 필수입니다'} />
+            <Error>{errors.time?.message}</Error>
           </div>
 
           <div className="info-content mt-[20px] mb-[10px]">
@@ -93,10 +138,17 @@ const Write = () => {
               id=""
               className="border outline-none text-xs resize-none w-full h-52 py-[5px] px-[10px] mt-[3px] rounded"
               placeholder="상품에 대한 설명을 적어주세요!"
+              {...register('text', { required: '* 내용은 필수입니다' })}
             />
-            <Error children={'* 내용은 필수입니다'} />
+            <Error>{errors.title?.message}</Error>
           </div>
-          <Button bg="main" color="white" height="40px" text="text-sm">
+          <Button
+            type="submit"
+            bg="main"
+            color="white"
+            height="40px"
+            text="text-sm"
+          >
             작성 완료
           </Button>
         </form>
