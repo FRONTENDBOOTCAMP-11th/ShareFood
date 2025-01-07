096451c66afb1c25e10ddd724f97901dba18b6ba
Youjiiin <yujinjang24@gmail.com>
2025-01-07 16:07:11 +0900

    ✨ 프로필 사진 변경 및 폼 제출

--- src/pages/myPage/modifyInfo.tsx
+++ src/pages/myPage/modifyInfo.tsx
@@ -7,14 +7,62 @@ import Layout from '../../components/Layout';
 
 import arrow from '/images/arrow/prevArrow.svg';
 import gallery from '/images/icons/gallery.svg';
+import { useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import { uploadImg } from '../../hooks/useUploadImg';
+
+type modifyInfoTypes = {
+  name: string;
+  profileImg: string | null;
+  phone: string;
+};
 
 const UserInfo = () => {
+  const [imgUrl, setImgUrl] = useState<string | null>(null);
   const apiUrl = import.meta.env.VITE_BASE_URL;
   const navigate = useNavigate();
 
   // 회원 정보 조회
   const { data: userInfo } = useGetUserInfo(1);
-  
+  useEffect(() => {
+    console.log(userInfo.item.image);
+    setImgUrl(userInfo.item.image);
+  }, [userInfo]);
+
+  // use-hook-form
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<modifyInfoTypes>({
+    defaultValues: {
+      name: userInfo.item.name,
+      profileImg: userInfo.item.image,
+      phone: userInfo.item.phone,
+    },
+  });
+
+  // 이미지 업로드 통신 + 이미지 src 바꿔 렌더링
+  const handleImgChange = async (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      try {
+        const filePath = await uploadImg([file]);
+        setImgUrl(`${filePath.path}`);
+        setValue('profileImg', filePath.name);
+      } catch (error) {
+        console.error('이미지 업로드 중 오류 발생:', error);
+      }
+    }
+  };
+
+  // 제출
+  const onSubmit = (data: modifyInfoTypes) => {
+    console.log(data);
+  };
 
   if (!userInfo) return <div>Loading...</div>;
 
@@ -33,23 +81,39 @@ const UserInfo = () => {
       </Header>
 
       <Layout>
-        <div className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+          {/* 프로필 이미지 */}
           <div className="relative w-[90px] h-[90px] m-auto">
             <img
-              src={`${apiUrl}${userInfo.item.image}`}
+              src={`${apiUrl}${imgUrl}`}
               alt="Profile"
               className="rounded-full w-full h-full"
             />
 
-            <button className="absolute top-0 left-0 w-full h-full rounded-full bg-transparent flex items-center justify-center">
+            <button
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                document.getElementById('fileInput')?.click();
+              }}
+              className="absolute top-0 left-0 w-full h-full rounded-full bg-transparent flex items-center justify-center"
+            >
               <img
                 src={gallery}
                 alt="Upload Icon"
                 className="w-[30px] h-[30px] absolute bottom-0 right-0 bg-[#969696] rounded-full p-[6px]"
               />
             </button>
+            <input
+              id="fileInput"
+              type="file"
+              accept="image/*"
+              onChange={handleImgChange}
+              className="hidden"
+            />
           </div>
 
+          {/* 닉네임 */}
           <div className="flex flex-col gap-[5px] relative">
             <label
               htmlFor="nickname"
@@ -58,12 +122,15 @@ const UserInfo = () => {
               닉네임
             </label>
             <input
+              {...register('name', { required: '닉네임을 입력해주세요.' })}
               type="text"
               id="nickname"
               placeholder="닉네임"
               className="border-b text-[13px] py-[3px]"
-              value={userInfo.item.name}
             />
+            {errors.name && (
+              <p className="text-error text-[10px]">{errors.name.message}</p>
+            )}
             <div className="absolute right-0 top-[60%] transform -translate-y-1/2">
               <Button
                 bg="main"
@@ -71,12 +138,14 @@ const UserInfo = () => {
                 text="text-[10px]"
                 width="60px"
                 height="25px"
+                type="button"
               >
                 중복체크
               </Button>
             </div>
           </div>
 
+          {/* 이메일(수정 불가) */}
           <div className="flex flex-col gap-[5px]">
             <label
               htmlFor="email"
@@ -89,6 +158,7 @@ const UserInfo = () => {
             </p>
           </div>
 
+          {/* 휴대전화 번호 */}
           <div className="flex flex-col gap-[5px] mb-[50px]">
             <label
               htmlFor="phone"
@@ -97,18 +167,34 @@ const UserInfo = () => {
               휴대전화 번호
             </label>
             <input
+              {...register('phone', {
+                required: '휴대전화 번호를 입력해주세요.',
+                pattern: {
+                  value: /^[0-9]{10,11}$/,
+                  message: '유효한 전화번호를 입력해주세요.',
+                },
+              })}
               type="text"
               id="phone"
               placeholder="휴대전화 번호"
               className="border-b text-[13px] py-[3px]"
               value={userInfo.item.phone}
             />
+            {errors.phone && (
+              <p className="text-error text-[10px]">{errors.phone.message}</p>
+            )}
           </div>
 
-          <Button height="40px" text="text-sm" bg="main" color="white">
+          <Button
+            height="40px"
+            text="text-sm"
+            bg="main"
+            color="white"
+            type="submit"
+          >
             정보 수정
           </Button>
-        </div>
+        </form>
       </Layout>
     </div>
   );
