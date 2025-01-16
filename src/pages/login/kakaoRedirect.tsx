d50b2375b9994d79568161de03b34e2b14922bc6
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2025-01-16 16:00:55 +0900

    💄 카카오 로그인 화면 개선

--- src/pages/login/kakaoRedirect.tsx
+++ src/pages/login/kakaoRedirect.tsx
@@ -1,8 +1,12 @@
 import { useNavigate } from 'react-router-dom';
-import useAxiosInstance from '../../hooks/useAxiosInstance';
 import { useEffect } from 'react';
 import { Slide, toast, ToastContainer } from 'react-toastify';
 import { useAuthStore } from '../../store/authStore';
+import { PulseLoader } from 'react-spinners';
+
+import useAxiosInstance from '../../hooks/useAxiosInstance';
+
+import forkChef from '/images/chef/forkChef.svg';
 
 const KakaoRedirect = () => {
   const navigate = useNavigate();
@@ -57,7 +61,25 @@ const KakaoRedirect = () => {
 
   return (
     <>
-      <p>로그인 처리 중입니다...</p>
+      <div className="kakao-login-loading">
+        <div className="flex flex-col justify-center items-center bg-main min-h-screen">
+          <div className="flex flex-col items-center">
+            <img className="size-[100px]" src={forkChef} alt="셰푸 이미지" />
+            <div className="flex flex-row items-center gap-1">
+              <p className="text-white font-semibold text-xl mt-1">
+                로그인 중입니다
+              </p>
+              <PulseLoader
+                color="#ffffff"
+                cssOverride={{}}
+                loading
+                size={10}
+                speedMultiplier={0.7}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
       <ToastContainer
         position="bottom-center"
         autoClose={1000}
